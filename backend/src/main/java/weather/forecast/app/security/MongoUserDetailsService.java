2bca04f256dc20428c8cc37fa19a8d38756cdfe6
noel-crimson <michal.kolezynski@gmail.com>
2026-03-18 17:15:29 +0100

    db and auth

---
+++ backend/src/main/java/weather/forecast/app/security/MongoUserDetailsService.java
@@ -0,0 +1,28 @@
+package weather.forecast.app.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import weather.forecast.app.user.User;
+import weather.forecast.app.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class MongoUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPasswordHash())
+                .roles(user.getRole())
+                .build();
+    }
+}
